e7ebd6a49f0be29269c751dc0368af5fc858c224
Ankur Saxena <ankur.ash.saxena@gmail.com>
2025-12-11 12:08:15 +0530

    updated model training parameters

--- train_model.py
+++ train_model.py
@@ -1,19 +1,16 @@
-# train_model.py
+# train_model_v2.py
 import pandas as pd
 import lightgbm as lgb
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, roc_auc_score
 import joblib
 import argparse
-import numpy as np
+from sklearn.metrics import accuracy_score, roc_auc_score
 
-FEATURES = ['ret_1','logret_1','ma_5','ma_15','ma_60','ma_5_15','vol_10','vol_60','rsi_14','bb_percent']
+FEATURES = ['ret_1','logret_1','ret_5','ret_15','ma_5','ma_15','ma_60','ma_5_15','vol_10','vol_60','atr_14','rsi_14','bb_percent']
 
-def train(infile='data/btc_features.csv', model_out='models/lgb_model.pkl'):
+def train(infile='data/btc_features.csv', model_out='models/lgb_model_v2.pkl'):
     df = pd.read_csv(infile, index_col=0, parse_dates=True)
     X = df[FEATURES]
     y = df['target']
-    # time-aware split: last 20% as test
     split = int(0.8 * len(df))
     X_train, X_test = X.iloc[:split], X.iloc[split:]
     y_train, y_test = y.iloc[:split], y.iloc[split:]
@@ -26,8 +23,7 @@ def train(infile='data/btc_features.csv', model_out='models/lgb_model.pkl'):
         'learning_rate': 0.05,
         'num_leaves': 31,
     }
-    bst = lgb.train(params, dtrain, num_boost_round=200)
-    # predict
+    bst = lgb.train(params, dtrain, num_boost_round=300)
     preds = bst.predict(X_test)
     preds_label = (preds > 0.5).astype(int)
     print("Accuracy:", accuracy_score(y_test, preds_label))
@@ -39,7 +35,7 @@ if __name__ == "__main__":
     import os
     parser = argparse.ArgumentParser()
     parser.add_argument('--infile', default='data/btc_features.csv')
-    parser.add_argument('--model_out', default='models/lgb_model.pkl')
+    parser.add_argument('--model_out', default='models/lgb_model_v2.pkl')
     args = parser.parse_args()
     os.makedirs('models', exist_ok=True)
     train(args.infile, args.model_out)
